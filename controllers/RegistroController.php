d791325b2055fdb4310dc1f9b94facc8ac3a1721
Juan Sebastian Candelo Balanta <jcandelobalanta@gmail.com>
2024-09-16 16:43:50 -0500

    Se implementó el botón de comprar las suscripciones con paypal

--- controllers/RegistroController.php
+++ controllers/RegistroController.php
@@ -7,41 +7,65 @@ use Model\Registro;
 use Model\Usuario;
 use MVC\Router;
 
-class RegistroController {
-    public static function crear(Router $router){
-        $router->render('registro/crear',[
+class RegistroController
+{
+    public static function crear(Router $router)
+    {
+        if (!isAuth()) {
+            header('Location: /');
+        }
+        //Verificar Si El Usuario Ya Tiene Un Plan
+        $registro = Registro::where('usuario_id', $_SESSION['id']);
+
+        if (isset($registro) && $registro->paquete_id === "3") {
+            header('Location: /boleto?id=' . urlencode($registro->token));
+        }
+        $router->render('registro/crear', [
             'titulo' => 'Finalizar Registro'
         ]);
     }
 
-    public static function gratis(Router $router){
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            if(!isAuth()){
+    public static function gratis(Router $router)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if (!isAuth()) {
                 header('Location: /login');
+            }
+            //Verificar Si El Usuario Ya Tiene Un Plan
+            $registro = Registro::where('usuario_id', $_SESSION['id']);
+
+            if (isset($registro) && $registro->paquete_id === "3") {
+                header('Location: /boleto?id=' . urlencode($registro->token));
+            }        //Verificar Si El Usuario Ya Tiene Un Plan
+            $registro = Registro::where('usuario_id', $_SESSION['id']);
+
+            if (isset($registro) && $registro->paquete_id === "3") {
+                header('Location: /boleto?id=' . urlencode($registro->token));
             }
             //Cortamos Los Primero Caracteres, Ya Que No Necesitamos Un Token Tan Largo
             $token = substr(md5(uniqid(rand(), true)), 0, 8);
             //Crear Registro
-            $datos = ['paquete_id'=>3, 'pago_id'=>'', 'token'=>$token, 'usuario_id'=>$_SESSION['id']];
+            $datos = ['paquete_id' => 3, 'pago_id' => '', 'token' => $token, 'usuario_id' => $_SESSION['id']];
 
             $registro = new Registro($datos);
             $resultado = $registro->guardar();
-            if($resultado){
+            if ($resultado) {
                 //UrlEncode Evita Caracteres Especiales
                 header('Location: /boleto?id=' . urlencode($registro->token));
             }
         }
     }
 
-    public static function boleto(Router $router){
+    public static function boleto(Router $router)
+    {
         //Validar URL
         $id = $_GET['id'];
-        if(!$id || !strlen($id) === 8){
+        if (!$id || !strlen($id) === 8) {
             header('Location: /');
         }
         //Buscar El Registro En La BD
         $registro = Registro::where('token', $id);
-        if(!$registro){
+        if (!$registro) {
             header('Location: /');
         }
         //Llenar Las Tablas De Referencia
@@ -49,7 +73,7 @@ class RegistroController {
         $registro->usuario = Usuario::find($registro->usuario_id);
         $registro->paquete = Paquete::find($registro->paquete_id);
 
-        $router->render('registro/boleto',[
+        $router->render('registro/boleto', [
             'titulo' => 'Asistencia A DevWebCamp',
             'registro' => $registro
         ]);
--- views/registro/crear.php
+++ views/registro/crear.php
@@ -26,6 +26,11 @@
                 <li class="paquete__elemento">Comida Y Bebida</li>
             </ul>
             <p class="paquete__precio">800.000 COP</p>
+            <div id="smart-button-container">
+                <div style="text-align: center;">
+                    <div id="paypal-button-container"></div>
+                </div>
+            </div>
         </div>
 
         <div class="paquete">
@@ -37,5 +42,55 @@
                 <li class="paquete__elemento">Acceso A Grabaciones</li>
             </ul>
             <p class="paquete__precio">200.000 COP</p>
-    </div>
+            <div id="smart-button-container">
+                <div style="text-align: center;">
+                    <div id="paypal-button-container"></div>
+                </div>
+            </div>
+        </div>
 </main>
+
+
+<!-- Reemplazar CLIENT_ID por tu client id proporcionado al crear la app desde el developer dashboard) -->
+<script src="https://www.paypal.com/sdk/js?client-id=AdmDeV5ElpFX21J-BayAPVSGTCZW7mfdX5E1etXIGm27VXQbKHyuT4p-VAdz0SJQaKJN_Qfuta9PKsAl" data-sdk-integration-source="button-factory"></script>
+ 
+<script>
+    function initPayPalButton() {
+      paypal.Buttons({
+        style: {
+          shape: 'rect',
+          color: 'blue',
+          layout: 'vertical',
+          label: 'pay',
+        },
+ 
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [{"description":"1","amount":{"currency_code":"USD","value":199}}]
+          });
+        },
+ 
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(orderData) {
+ 
+            // Full available details
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+ 
+            // Show a success message within this page, e.g.
+            const element = document.getElementById('paypal-button-container');
+            element.innerHTML = '';
+            element.innerHTML = '<h3>Thank you for your payment!</h3>';
+ 
+            // Or go to another URL:  actions.redirect('thank_you.html');
+            
+          });
+        },
+ 
+        onError: function(err) {
+          console.log(err);
+        }
+      }).render('#paypal-button-container');
+    }
+ 
+  initPayPalButton();
+</script>
